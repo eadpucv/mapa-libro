7b4e60e2f8053764d8ac3e155a4d6050aca6848e
Herbert Spencer <hspencer@ead.cl>
2022-10-02 12:48:15 -0300

    ajustes de ancho (-10)

--- js/big-map.js
+++ js/big-map.js
@@ -124,7 +124,7 @@ function buildChapters() {
 
 function setup() {
     //let w = document.getElementById("p5").offsetWidth;
-    mapa = createCanvas(windowWidth, windowHeight);
+    mapa = createCanvas(windowWidth - 10, windowHeight);
     mapa.parent('p5js');
 
     engine = Engine.create();
